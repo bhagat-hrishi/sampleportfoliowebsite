f49a90d66d8c923327a407faa0bf86e121220fcb
bhagat-hrishi <hrishibhagat8642@gmail.com>
2020-11-05 19:58:57 +0530

    change in dropdown

--- js/app.js
+++ js/app.js
@@ -10,11 +10,16 @@ function dropDownFunction(){
     {
         // console.log(navUnorderedList.classList)
         dropDown.innerHTML = ` CLOSE <i class="fa fa-close"></i> `
+        dropDown.style.color ='#3498db';
+        dropDown.style.backgroundColor = 'white';
+
     }else {
         // console.log(navUnorderedList.classList)
         dropDown.innerHTML=`
             <i class="fa fa-bars" style="font-size: 3vh;"></i>
         `
+         dropDown.style.color ='white';
+        dropDown.style.backgroundColor = '#3498db';
     }    
 }   
 
